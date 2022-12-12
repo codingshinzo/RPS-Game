271eecd3c9ec904c2c8a3f0325929f03b1e6b6fb
bhagyashri <98913801+codingshinzo@users.noreply.github.com>
2022-12-12 23:02:37 +0530

    Update rps.java

--- rps.java
+++ rps.java
@@ -1,14 +1,11 @@
 package RPS;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.Random;
-import java.util.Scanner;
 
-import javax.swing.*;
 
 public class rps implements ActionListener {
 
